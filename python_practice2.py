8cf9ba916621bc002414f7567cd8053ea1e09295
wonyoung1217 <petercwy@naver.com>
2022-10-12 22:01:44 +0900

    python_practice2 modification on Branch_first.

--- python_practice2.py
+++ python_practice2.py
@@ -8,13 +8,13 @@ def sinusoid(A, f0, phi, fs, t):
 	# 위 아래 둘중 아무거나 사용하면 됨
 	# 점 갯수를 fs개로
 	# t=np.linspace(0, t, int(fs)) 
-	x=A*np.cos(2*np.pi*f0*t*+phi)
+	x=A*np.sin(2*np.pi*f0*t*+phi)
 	return x
     
 # 지속시간 t를 짧게 해야 구불구불이 보인다
 sin_A=sinusoid(A=0.8, f0=440, phi=np.pi/2, fs=44100, t=0.005)
 
-plt.plot(sin_A)
+plt.plot(cos_A)
 plt.xlabel("time")
 plt.ylabel("amplitude")
 plt.show()
